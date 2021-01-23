1d9c6cc7a112e8896524593ecf6836b2e0e84af8
KrunalSisodia <krunalsisodia@gmail.com>
2021-01-23 21:16:29 +0530

    create dataLayer and reducer for spotify dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import "./assets/css/App.css";
 import { getTokenFromUrl } from "./assets/js/Spotify";
 import Login from "./components/Login";
+import Player from "./components/Player";
 
+const spotify = new SpotifyWebApi();
 function App() {
   const [token, setToken] = useState(null);
 
@@ -12,12 +15,14 @@ function App() {
     const _token = hash.access_token;
     if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
     }
   }, []);
 
-  return (
-    <div className="App">{token ? <h1>I am logged in</h1> : <Login />}</div>
-  );
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
+import { DataLayer } from "./components/Datalayer";
+import reducer, { initialState } from "./components/reducer";
 
-ReactDom.render(<App />, document.querySelector("#root"));
+ReactDom.render(
+  <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </React.StrictMode>,
+  document.querySelector("#root")
+);
